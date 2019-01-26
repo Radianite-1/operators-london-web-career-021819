1f5d560a2fa935cb08de403a283603e198c2a3c5
Luke Ojukwu <luke.ojukwu@gmail.com>
2019-01-26 23:23:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -16,8 +16,13 @@ end
 
 def not_safe?(speed)
 
+<<<<<<< HEAD
 speed > 60 || speed < 40 ? true : false
 
+=======
+speed > 40 || speed < 60 ? return TRUE
+	
+>>>>>>> 69686fa5bc919ca982208f2cd4b36be096bde2a0
 end
 	
 
